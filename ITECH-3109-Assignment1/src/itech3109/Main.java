c746121e26a801874574f8be2fffeab944fa2619
BBGard <gonzogardiner@gmail.com>
2023-08-06 21:30:06 +1000

    Initial commit

---
+++ ITECH-3109-Assignment1/src/itech3109/Main.java
@@ -0,0 +1,15 @@
+package itech3109;
+
+/**
+ * @Author: Benjamin Gardiner - 30399545
+ * 
+ * This is the main class for running the ITECH-3109 Assignment 1 code.
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
