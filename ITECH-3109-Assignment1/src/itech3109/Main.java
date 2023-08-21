a28b1b1d42b01041099e8ecfe934fe7ed430e04b
BBGard <gonzogardiner@gmail.com>
2023-08-21 16:33:55 +1000

    Begin

--- ITECH-3109-Assignment1/src/itech3109/Main.java
+++ ITECH-3109-Assignment1/src/itech3109/Main.java
@@ -10,7 +10,34 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] testArray = {1,2,3};
+		
+		nextPermutation(testArray);
 
+	}
+	
+	private static boolean nextPermutation(int[] permutation) {
+		int length = permutation.length;
+		int max;
+		
+		// If n=ai, i>1 aka array has more than 1 element
+		if(length > 1 && permutation[0] != max) {
+			// Swap ai and ai-1, return true
+			int temp = permutation[length-2];
+			permutation[length-2] = permutation[length-1];
+			permutation[length-1] = temp;
+			
+			printArray(permutation);
+			return true;	
+		}
+		
+		return false;
+	}
+	
+	private static void printArray(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
 	}
 
 }
